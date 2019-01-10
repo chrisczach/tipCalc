dd0d2e2adb7fd8806ea53e21198cf1943c8fce4d
Christopher Czach <chrisczach@gmail.com>
2019-01-09 20:04:20 -0800

    .

---
+++ Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello from Hello.js</Text>
+      </View>
+    );
+  }
+}
