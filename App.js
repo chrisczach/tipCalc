beec0253182aa9469c1058f4cc89baa9ce72f2a0
Christopher Czach <chrisczach@gmail.com>
2019-01-10 21:24:14 -0800

    .

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Hello from './Hello';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { Button, StyleSheet, View, TextInput, Text } from 'react-native';
+import { number } from 'prop-types';
 
 export default class App extends React.Component {
+  state = {
+    inputValue: '',
+    tipPercent: 0.15
+  };
+
   render() {
+    const { inputValue, tipPercent } = this.state;
+    const { container, input } = styles;
+    const tip = (amount = 0, percent = 0.15) => amount * percent;
+
     return (
-      <View style={styles.container}>
-        <TextInput style={styles.input}></TextInput>
+      <View style={container}>
+        <Text>${tip(inputValue, tipPercent).toFixed(2)}</Text>
+        <TextInput
+          keyboardType="numeric"
+          placeholder="0.00"
+          onChangeText={inputValue => this.setState({ inputValue })}
+          value={inputValue}
+          style={input}
+        />
+        <View>
+          <Button
+          />
+          <Button />
+        </View>
       </View>
     );
   }
